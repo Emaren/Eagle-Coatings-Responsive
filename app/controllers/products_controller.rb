df79bb734c6870a017c84ebeaec44da2f15c6412
Tony <tonyblum@me.com>
2016-05-07 20:42:04 -0700

    initial commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,85 @@
+class ProductsController < ApplicationController
+  def index
+  end
+
+  def supertherm
+  end
+
+  def sunshield
+  end
+
+  def epoxotherm
+  end
+
+  def hotsurface
+  end
+
+  def hotpipe
+  end
+
+  def rustgrip
+  end
+
+  def rustgrip1000
+  end
+
+  def rustgripe
+  end
+
+  def moistmetalgrip
+  end
+
+  def liningkote
+  end
+
+  def omegafire
+  end
+
+  def penetratingchemical
+  end
+
+  def interiorlatex
+  end
+
+  def polyurethane
+  end
+
+  def safecoat
+  end
+
+  def spinterlock
+  end
+
+  def superbase
+  end
+
+  def totalseal
+  end
+
+  def totalsealhs
+  end
+
+  def liquidmembrane
+  end
+
+  def enamogripcolour
+  end
+
+  def enamogripclear
+  end
+
+  def enamogripclear1000
+  end
+
+  def enamogripclear3700
+  end
+
+  def enamogripclear5000
+  end
+
+  def aquapox
+  end
+
+  def graffiticleaner
+  end
+end
---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,144 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get supertherm" do
+    get :supertherm
+    assert_response :success
+  end
+
+  test "should get sunshield" do
+    get :sunshield
+    assert_response :success
+  end
+
+  test "should get epoxotherm" do
+    get :epoxotherm
+    assert_response :success
+  end
+
+  test "should get hotsurface" do
+    get :hotsurface
+    assert_response :success
+  end
+
+  test "should get hotpipe" do
+    get :hotpipe
+    assert_response :success
+  end
+
+  test "should get rustgrip" do
+    get :rustgrip
+    assert_response :success
+  end
+
+  test "should get rustgrip1000" do
+    get :rustgrip1000
+    assert_response :success
+  end
+
+  test "should get rustgripe" do
+    get :rustgripe
+    assert_response :success
+  end
+
+  test "should get moistmetalgrip" do
+    get :moistmetalgrip
+    assert_response :success
+  end
+
+  test "should get liningkote" do
+    get :liningkote
+    assert_response :success
+  end
+
+  test "should get omegafire" do
+    get :omegafire
+    assert_response :success
+  end
+
+  test "should get penetratingchemical" do
+    get :penetratingchemical
+    assert_response :success
+  end
+
+  test "should get interiorlatex" do
+    get :interiorlatex
+    assert_response :success
+  end
+
+  test "should get polyurethane" do
+    get :polyurethane
+    assert_response :success
+  end
+
+  test "should get safecoat" do
+    get :safecoat
+    assert_response :success
+  end
+
+  test "should get spinterlock" do
+    get :spinterlock
+    assert_response :success
+  end
+
+  test "should get superbase" do
+    get :superbase
+    assert_response :success
+  end
+
+  test "should get totalseal" do
+    get :totalseal
+    assert_response :success
+  end
+
+  test "should get totalsealhs" do
+    get :totalsealhs
+    assert_response :success
+  end
+
+  test "should get liquidmembrane" do
+    get :liquidmembrane
+    assert_response :success
+  end
+
+  test "should get enamogripcolour" do
+    get :enamogripcolour
+    assert_response :success
+  end
+
+  test "should get enamogripclear" do
+    get :enamogripclear
+    assert_response :success
+  end
+
+  test "should get enamogripclear1000" do
+    get :enamogripclear1000
+    assert_response :success
+  end
+
+  test "should get enamogripclear3700" do
+    get :enamogripclear3700
+    assert_response :success
+  end
+
+  test "should get enamogripclear5000" do
+    get :enamogripclear5000
+    assert_response :success
+  end
+
+  test "should get aquapox" do
+    get :aquapox
+    assert_response :success
+  end
+
+  test "should get graffiticleaner" do
+    get :graffiticleaner
+    assert_response :success
+  end
+
+end
