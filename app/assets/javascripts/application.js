cdb8c4143a679657b05b96af33bf897a67f31422
Tony <tonyblum@me.com>
2016-05-14 19:34:36 -0700

    Product Architecture completed, documents completed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,82 +21,107 @@ $(document).ready(function(){
   //   $( '.thermal-parallax' ).show( "slow" );
   // });
 
-  $("img[src='/assets/co.png'], .corrosion-text").click(function() {
-    $( '.corrosion-parallax' ).show( "slow" );
-  });
-
-  $("img[src='/assets/fire.png'], .fire-text").click(function() {
-    $( '.fire-parallax' ).show( "slow" );
-  });
-
+  // $("img[src='/assets/co.png'], .corrosion-text").click(function() {
+  //   $( '.corrosion-parallax' ).show( "slow" );
+  // });
+  //
+  // $("img[src='/assets/fire.png'], .fire-text").click(function() {
+  //   $( '.fire-parallax' ).show( "slow" );
+  // });
+  //
 
 
   $("img[src='/assets/thermal.png'], .thermal-text").click(function(){
     $("img[src='/assets/co.png'], .corrosion-text").fadeOut({
-      opacity: .5,
-    }, 1500 );
-    $("img[src='/assets/fire.png'], .fire-text").fadeOut({
-      opacity: .5,
-    }, 1500 );
-    $("img[src='/assets/thermal.png'], .thermal-text").animate({
-      left: "+=24%"
     }, 1000 );
-    $("p.intro").fadeOut({
-      opacity: .5,
-    }, 1500 );
-    $(".mainFooter").fadeOut({
-      opacity: .5,
-    }, 1500 );
+    $("img[src='/assets/fire.png'], .fire-text").fadeOut({
+    }, 1000 );
+    $("img[src='/assets/thermal.png'], .thermal-text").animate({
+      left: "+=31%"
+    }, 1000 );
+    $("p.intro").hide();
+    $(".mainFooter").hide();
     $("img[src='/assets/phone.png']").fadeOut({
-      opacity: .5,
     }, 1500 );
-    $(".thermal-parallax").show('scale', 2500);
-    // $("p.intro").hide('clip', 2000);
-    // $(".footer-container").hide('clip', 2000);
-    // $(".page-opening").show('scale', 1900);
-    // $(".page-opening-bottom").show('scale', 1900);
+    $(".thermal-parallax").show('scale', 2000);
   });
 
 
-$(".corrosion-img-title").click(function(){
-  $(".thermal-img-title").fadeOut(1000)
-  $(".thermal-white").fadeOut(1000)
-  $(".thermal-box").fadeOut(1000)
-  $(".thermal-img").fadeOut(1000)
-  $(".thermal-vid").fadeOut(1000)
-  $(".play-thermal").fadeOut(1000)
-  $(".fire-img-title").fadeOut(1000)
-  $(".fire-white").fadeOut(1000)
-  $(".fire-box").fadeOut(1000)
-  $(".fire-img").fadeOut(1000)
-  $(".fire-vid").fadeOut(1000)
-  $(".play-fire").fadeOut(1000)
-  $(".corrosion-img-title").animate({
-    height: "100%",
-    width: "100%",
-  }, 300 );
-        $(".corrosion-white").animate({
+  $("img[src='/assets/co.png'], .corrosion-text").click(function(){
+    $("img[src='/assets/thermal.png'], .thermal-text").fadeOut({
+    }, 1000 );
+    $("img[src='/assets/fire.png'], .fire-text").fadeOut({
+    }, 1000 );
+    $("img[src='/assets/co.png'], .corrosion-text").animate({
+    width: "30%",
+  }, 1500 );
+    $("p.intro").hide();
+    $(".mainFooter").hide();
+    $("img[src='/assets/phone.png']").fadeOut({
+    }, 1500 );
+    $( '.corrosion-parallax' ).show('scale', 2000);
+  });
 
-  }, 300 );
-        $(".corrosion-box").animate({
+function hideThermal() {
+  document.getElementById("#thermal").style.visibility = "hidden";
+}
 
-  }, 300 );
-        $(".corrosion-img").animate({
-    height: "100%",
-    width: "100%",
-  }, 300 );
-        $(".corrosion-vid, .play-corrosion").animate({
-    height: "100%",
-    width: "100%",
-  }, 300 );
-        $(".play-corrosion").animate({
-    height: "100%",
-    width: "100%",
-  }, 300 );
-  $(".intro").hide('scale', 400);
-  $(".footer-container").hide('scale', 100);
-  $(".thermal-parallax").show('scale', 2500);
-});
+
+  // $("img[src='/assets/fire.png'], .fire-text").click(function(){
+  //   $("img[src='/assets/co.png'], .corrosion-text").fadeOut({
+  //   }, 1000 );
+  //   $("img[src='/assets/fire.png'], .fire-text").animate({
+  //     // left: "-=31%",
+  //   }, 1000 );
+  //   $("img[src='/assets/thermal.png'], .thermal-text").fadeOut({
+  //   }, 1000 );
+  //   $("p.intro").hide();
+  //   $(".mainFooter").hide();
+  //   $("img[src='/assets/phone.png']").fadeOut({
+  //   }, 1500 );
+  //   $(".fire-parallax").show('scale', 2000);
+  // });
+
+
+// $(".corrosion-img-title").click(function(){
+//   $(".thermal-img-title").fadeOut(1000)
+//   $(".thermal-white").fadeOut(1000)
+//   $(".thermal-box").fadeOut(1000)
+//   $(".thermal-img").fadeOut(1000)
+//   $(".thermal-vid").fadeOut(1000)
+//   $(".play-thermal").fadeOut(1000)
+//   $(".fire-img-title").fadeOut(1000)
+//   $(".fire-white").fadeOut(1000)
+//   $(".fire-box").fadeOut(1000)
+//   $(".fire-img").fadeOut(1000)
+//   $(".fire-vid").fadeOut(1000)
+//   $(".play-fire").fadeOut(1000)
+//   $(".corrosion-img-title").animate({
+//     height: "100%",
+//     width: "100%",
+//   }, 300 );
+//         $(".corrosion-white").animate({
+//
+//   }, 300 );
+//         $(".corrosion-box").animate({
+//
+//   }, 300 );
+//         $(".corrosion-img").animate({
+//     height: "100%",
+//     width: "100%",
+//   }, 300 );
+//         $(".corrosion-vid, .play-corrosion").animate({
+//     height: "100%",
+//     width: "100%",
+//   }, 300 );
+//         $(".play-corrosion").animate({
+//     height: "100%",
+//     width: "100%",
+//   }, 300 );
+//   $(".intro").hide('scale', 400);
+//   $(".footer-container").hide('scale', 100);
+//   $(".thermal-parallax").show('scale', 2500);
+// });
 
 
 
@@ -136,6 +161,8 @@ $(".fire-img-title").click(function(){
   $(".footer-container").hide('scale', 500);
   $(".thermal-parallax").show('scale', 1000);
 });
+});
 
-
+$(".search").click(function(){
+  $(".product-inventory").fadeOut(1000);
 });
